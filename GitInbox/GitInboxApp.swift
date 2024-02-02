c71d8a9410d3a6c41df675ed588cab56e9b2efca
yusuf u <yusuf.uzan>
2024-02-03 00:39:31 +0300

    File organize

---
+++ GitInbox/GitInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInboxApp.swift
+//  GitInbox
+//
+//  Created by Yusuf Uzan on 3.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
